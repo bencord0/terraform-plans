9d5fe481b70123cd7c797a055b178d287e74b0a6
Ben Cordero <bencord0@condi.me>
2017-08-05 08:52:51 +0100

    Enable hostnames in VPC
    With some ssh trickery, jump directly to VPC instances.
    
        $ ssh ip-xxx-xxx-xxx-xxx.eu-west2.compute.internal
    
    This can be done with a ssh config ProxyJump directive
    
    Host *.compute.internal
        User core
        IdentityFile "~/.ssh/id_rsa"
        ProxyJump core@<bastion>

--- eu-west-2/10-vpc.tf
+++ eu-west-2/10-vpc.tf
@@ -1,6 +1,11 @@
 # Everything starts with a VPC
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
+
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  assign_generated_ipv6_cidr_block = true
 }
 
 # Grant the VPC internet access (NAT)
--- eu-west-2/scripts/list_asg_instances.sh
+++ eu-west-2/scripts/list_asg_instances.sh
@@ -3,5 +3,5 @@
 instance_ids="$(aws autoscaling describe-auto-scaling-groups| jq -r '.AutoScalingGroups[].Instances[].InstanceId')"
 
 for iid in $instance_ids; do
-  aws ec2 describe-instances --instance-ids $iid | jq -r '.Reservations[].Instances[].PrivateIpAddress'
+  aws ec2 describe-instances --instance-ids $iid | jq -r '.Reservations[].Instances[].PrivateDnsName'
 done
