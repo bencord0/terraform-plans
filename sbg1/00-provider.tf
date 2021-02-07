04d444d58479d6c039644c67cf393e8c87a5806c
Ben Cordero <bencord0@condi.me>
2021-02-07 14:18:40 +0000

    sbg1: Update provider and backend config

--- sbg1/00-provider.tf
+++ sbg1/00-provider.tf
@@ -1,11 +1,12 @@
 provider "openstack" {
-  auth_url = "https://auth.cloud.ovh.net/v2.0/"
-  region = "SBG1"
+  auth_url = "https://auth.cloud.ovh.net/v3"
+  region   = "SBG1"
 }
 
 terraform {
   backend "consul" {
-    address = "https://consul.condi.me"
-    path = "sbg1/terraform_state"
+    address = "consul.condi.me"
+    scheme  = "https"
+    path    = "sbg1/terraform_state"
   }
 }
--- sbg1/30-security.tf
+++ sbg1/30-security.tf
@@ -10,7 +10,7 @@ resource "openstack_networking_secgroup_rule_v2" "httpv4" {
   port_range_min = 80
   port_range_max = 80
   remote_ip_prefix = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.web.id}"
+  security_group_id = openstack_networking_secgroup_v2.web.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "httpsv4" {
@@ -20,7 +20,7 @@ resource "openstack_networking_secgroup_rule_v2" "httpsv4" {
   port_range_min = 443
   port_range_max = 443
   remote_ip_prefix = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.web.id}"
+  security_group_id = openstack_networking_secgroup_v2.web.id
 }
 
 resource "openstack_networking_secgroup_v2" "ssh" {
@@ -35,6 +35,6 @@ resource "openstack_networking_secgroup_rule_v2" "sshv4" {
   port_range_min = 22
   port_range_max = 22
   remote_ip_prefix = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.ssh.id}"
+  security_group_id = openstack_networking_secgroup_v2.ssh.id
 }
 
