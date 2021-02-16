071b844be95eb526fcc2a6a30683aeb80c10014f
Ben Cordero <bencord0@condi.me>
2021-02-16 21:48:27 +0000

    Update security groups to include v6 rules

--- sbg1/30-security.tf
+++ sbg1/30-security.tf
@@ -1,40 +1,69 @@
 resource "openstack_networking_secgroup_v2" "web" {
-  name = "web"
+  name        = "web"
   description = "Allow HTTP/HTTPS traffic"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "httpv4" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol = "tcp"
-  port_range_min = 80
-  port_range_max = 80
-  remote_ip_prefix = "0.0.0.0/0"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.web.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "httpv6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "::/0"
   security_group_id = openstack_networking_secgroup_v2.web.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "httpsv4" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol = "tcp"
-  port_range_min = 443
-  port_range_max = 443
-  remote_ip_prefix = "0.0.0.0/0"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.web.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "httpsv6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "::/0"
   security_group_id = openstack_networking_secgroup_v2.web.id
 }
 
 resource "openstack_networking_secgroup_v2" "ssh" {
-  name = "ssh"
+  name        = "ssh"
   description = "Allow SSH traffic"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "sshv4" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol = "tcp"
-  port_range_min = 22
-  port_range_max = 22
-  remote_ip_prefix = "0.0.0.0/0"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.ssh.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "sshv6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "::/0"
+  security_group_id = openstack_networking_secgroup_v2.ssh.id
+}
